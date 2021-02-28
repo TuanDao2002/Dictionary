adc308b04598de691e0e4f2e001a7e1a499134a0
TuanDao2002 <s3877347@rmit.edu.vn>
2021-02-28 21:05:04 +0700

    update response

--- helperFunctions.js
+++ helperFunctions.js
@@ -4,20 +4,30 @@ const render = (res) => {
     }
 
     if(!res.length){
-        responseField.innerHTML = "<p>Try again!</p><p>There is no definition for this word</p>";
+        responseField.innerHTML = "<p id='warning'>Try again!</p><p id='warning'>There is no definition for this word</p>";
     }
-
-    let wordDefinitions = [];
-    const len = res[0].meanings.length;
-
-    for (let i = 0; i < len; i++){
-        const partOfSpeech = res[0].meanings[i].partOfSpeech;
-        const definition = res[0].meanings[i].definitions[0].definition;
-        wordDefinitions.push(`<li>${partOfSpeech} -> ${definition}</li>`);
+    if (res[0].word === 'undefined'){
+        responseField.innerHTML = "<p id='warning'>Try again!</p><p id='warning'>You have not typed a word</p>";
+    } else {
+        let wordDefinitions = [];
+        const len = res[0].meanings.length;
+    
+        for (let i = 0; i < len; i++){
+            const partOfSpeech = res[0].meanings[i].partOfSpeech;
+            wordDefinitions.push(`<p id="partOfSpeech">+ ${partOfSpeech}:</p>`);
+            const definitions = res[0].meanings[i].definitions;
+            definitionsArray = []
+            definitions.forEach(element => {
+                definitionsArray.push(`<p>--> ${element.definition}<p>`);
+            });
+            definitionsArray = definitionsArray.join("")
+            wordDefinitions.push(definitionsArray);
+        }
+    
+        wordDefinitions = wordDefinitions.join("")
+        responseField.innerHTML = `<p id="def">Definition</p><ul>${wordDefinitions}</ul>`;
+        console.log(res[0].word);
     }
-
-    wordDefinitions = wordDefinitions.join("")
-    responseField.innerHTML = `<p>Definition:</p><ul>${wordDefinitions}</ul>`;
-    console.log(res);
+    
 }
 
